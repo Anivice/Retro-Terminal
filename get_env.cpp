9665d8c95936bc16f4b5068ebcfe3108c0898e66
anivice <AniviceIves@gmail.com>
2025-06-22 11:29:29 +0800

    added backtrace level 1

--- get_env.cpp
+++ get_env.cpp
@@ -1,8 +1,9 @@
 #include "get_env.h"
+#include <cstdlib>
 
 std::string get_env(const std::string & name)
 {
-    const char * ptr = std::getenv(name.c_str());
+    const char * ptr = secure_getenv(name.c_str());
     if (ptr == nullptr)
     {
         return "";
