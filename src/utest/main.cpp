4e5554f4161cc8dc64b779db054b03190479ab3e
anivice <AniviceIves@gmail.com>
2025-06-30 09:13:17 +0800

    better terminal handling (inside a pipe with no terminal)

--- src/utest/main.cpp
+++ src/utest/main.cpp
@@ -6,22 +6,60 @@
 #include <cstring>
 #include <ranges>
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <vector>
 #include <algorithm>
 #include "test/test.h"
 #include "helper/color.h"
+#include "helper/cpp_assert.h"
 
 extern void show();
 
 int main(int argc, char *argv[])
 {
-    std::cout << "\x1b[?25l"; // hide cursor
+    auto list_all_tests = []()
+    {
+        std::cout << color::color(5,2,0) << "Available unit tests are:" << color::no_color() << std::endl;
+        int display_len = 0;
+        for (const auto & id: test::unit_tests | std::views::keys) {
+            if (display_len < static_cast<int>(id.length())) {
+                display_len = static_cast<int>(id.length());
+            }
+        }
+
+        display_len += 4;
+
+        for (const auto & [id, base] : test::unit_tests)
+        {
+            std::string case_insensitive_test_name = id;
+            std::ranges::transform(case_insensitive_test_name, case_insensitive_test_name.begin(), ::tolower);
+            std::cout << color::color(5,1,1) << "   " << id
+                << std::string(std::max(display_len - static_cast<int>(id.length()), 4), ' ')
+                << color::color(5,2,2) << ((test::unit_t*)base)->name() << color::no_color() << std::endl;
+        }
+    };
+
+    if (argc == 2 && std::string(argv[1]) == "list")
+    {
+        list_all_tests();
+        return EXIT_SUCCESS;
+    }
+
     int current_test = 1;
     const auto all_tests = test::unit_tests.size();
     int success = 0;
     int failed = 0;
     bool selective_test = argc > 1;
     int selective_count = 0;
+    bool is_terminal = false;
+    struct stat st{};
+    assert_short(fstat(STDOUT_FILENO, &st) != -1);
+    if (isatty(STDOUT_FILENO))
+    {
+        is_terminal = true;
+    }
+
+    if (is_terminal) std::cout << "\x1b[?25l"; // hide cursor
     for (const auto & [test_id, unit_address] : test::unit_tests)
     {
         if (argc > 1)
@@ -46,22 +84,25 @@ int main(int argc, char *argv[])
         }
 
         auto * base_unit = (test::unit_t *)unit_address;
-        int length = 4;
+        int length = 48;
 
-        winsize w{};
-        // If redirecting STDOUT to one file ( col or row == 0, or the previous
-        // ioctl call's failed )
-        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != 0 ||
-            (w.ws_col | w.ws_row) == 0)
+        if (is_terminal)
         {
-            std::cerr << "Warning: failed to determine a reasonable terminal size: " << strerror(errno) << std::endl;
-        } else {
-            length = static_cast<int>(w.ws_col / 2.5);
-            // std::cout << length << std::endl;
+            winsize w{};
+            // If redirecting STDOUT to one file ( col or row == 0, or the previous
+            // ioctl call's failed )
+            if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != 0 ||
+                (w.ws_col | w.ws_row) == 0)
+            {
+                std::cerr << "Warning: failed to determine a reasonable terminal size: " << strerror(errno) << std::endl;
+            } else {
+                length = static_cast<int>(w.ws_col / 2.5);
+                // std::cout << length << std::endl;
+            }
         }
 
         std::stringstream output_head;
-        output_head << "\x1b[2K\r" << color::color(1,5,4) << "TEST\t[" << current_test++ << "/"
+        output_head << (is_terminal ? "\x1b[2K\r" : "") << color::color(1,5,4) << "TEST\t[" << current_test++ << "/"
                     << (selective_test ? argc - 1 : all_tests) << "]\t"
                     << base_unit->name() << std::string(std::max(length - static_cast<signed int>(base_unit->name().length()), 4), ' ');
         const auto output_str = output_head.str();
@@ -81,7 +122,7 @@ int main(int argc, char *argv[])
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
             counter++;
-            if (counter == 100) {
+            if (counter == 100 && is_terminal) {
                 std::cout << output_str << "  " << color::color(2,3,4) << ++seconds << " s" << color::no_color() << std::flush;
                 counter = 0;
             }
@@ -97,7 +138,9 @@ int main(int argc, char *argv[])
         }
 
         if (result) {
-            std::cout << output_str << color::color(0,5,0) << "  PASSED: " << color::color(0,2,2) << base_unit->success() << "\r" << std::flush;
+            std::cout << (is_terminal ? output_str : std::string()) << color::color(0,5,0)
+                << "  PASSED: " << color::color(0,2,2) << base_unit->success()
+                << (is_terminal ? "\r" : "\n") << std::flush;
             success++;
         } else {
             std::cout << output_str << "  " << color::color(5,0,0) << color::bg_color(0,0,1)
@@ -113,15 +156,20 @@ int main(int argc, char *argv[])
               << color::color(0,5,0) << "    " << success << " PASSED\n"
               << (failed == 2 ? color::color(2,0,0) : color::color(5,0,0)) << "    "
               << failed << (selective_test ? " FAILED\n" : " FAILED (2 DESIGNED TO FAIL)\n") << color::no_color();
-    std::cout << "\x1b[?25h" << std::endl; // show cursor
+    std::cout << (is_terminal ? "\x1b[?25h" : "") << std::endl; // show cursor
 
     if (failed == 2 || (selective_test && failed == 0 && selective_count == (argc - 1)))
     {
         std::cout << color::color(0,5,0)
                   << "Congratulations, all unit tests passed" << (selective_test ? "" : " (with 2 designed to fail)")
                   << color::no_color() << std::endl << std::endl << std::flush;
-        show();
-        std::cout << std::endl;
+
+        if (is_terminal)
+        {
+            show();
+            std::cout << std::flush << std::endl;
+        }
+
         return EXIT_SUCCESS;
     }
 
@@ -208,25 +256,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        // 3. print help
-        std::cout << color::color(5,2,0) << "Available unit tests are:" << color::no_color() << std::endl;
-        int display_len = 0;
-        for (const auto & id: test::unit_tests | std::views::keys) {
-            if (display_len < static_cast<int>(id.length())) {
-                display_len = static_cast<int>(id.length());
-            }
-        }
-
-        display_len += 4;
-
-        for (const auto & [id, base] : test::unit_tests)
-        {
-            std::string case_insensitive_test_name = id;
-            std::ranges::transform(case_insensitive_test_name, case_insensitive_test_name.begin(), ::tolower);
-            std::cout << color::color(5,1,1) << "   " << id
-                << std::string(std::max(display_len - static_cast<int>(id.length()), 4), ' ')
-                << color::color(5,2,2) << ((test::unit_t*)base)->name() << color::no_color() << std::endl;
-        }
+        list_all_tests();
     }
 
     return EXIT_FAILURE;
